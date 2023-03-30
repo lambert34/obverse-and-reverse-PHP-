c93d3b417b46c2c894fec456f32b50c070922aa6
lambert34 <aleksandrlambert@gmail.com>
2023-03-30 15:33:52 +0700

    создал классы и конструкцию, которая будет их принимать

--- php-project/index.php
+++ php-project/index.php
@@ -1,5 +1,15 @@
 <?php
 
+class Game {
+    protected $player1;
+    protected $player1;
+}
+
+public function __construct(Player $p)
+{
+    
+}
+
 $game = new Game(
     new Player("Alex", 100),
     new Player("Alice", 100)
